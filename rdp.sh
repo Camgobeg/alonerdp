d00e6defde2345054708f63d5097084e27a849ad
Camgobeg <camgobeg@gmail.com>
2025-01-19 13:21:33 +0300

    Update rdp.sh

--- rdp.sh
+++ rdp.sh
@@ -5,5 +5,5 @@ sudo dpkg -i cloudflared-linux-amd64.deb
 Xvfb :99 -screen 0 1280x800x24 &
 DISPLAY=:99 startxfce4 &
 x11vnc -display :99 -rfbport 5901 -forever -nopw &
-websockify --web=/usr/share/novnc 6080 localhost:5901
 cloudflared tunnel --url http://localhost:6080
+websockify --web=/usr/share/novnc 6080 localhost:5901
